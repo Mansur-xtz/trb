a67f645db0d010849b725f60ef3050f8c8d612e1
Mansur-xtz <144968802+Mansur-xtz@users.noreply.github.com>
2024-11-06 11:49:18 -0300

    Add files via upload

---
+++ trb-main/login/index.php
@@ -0,0 +1,49 @@
+<?php
+
+ob_start(); // Inicia o buffer de saída
+session_start(); // Inicia a sessão para armazenar o login
+require __DIR__. "/app/model/Userbanco.php";
+require __DIR__ . "/app/Controller/ValidarUsuario.php";
+require __DIR__ . "/app/Controller/CadastrarUsuario.php";
+require __DIR__ . "/app/Controller/ExcluirUsuario.php";
+require __DIR__ . "/header.php";
+
+
+
+if (!isset($_POST['usuario']) || !isset($_POST['senha'])) {
+    header("Location: ./inicio.php");
+    exit();
+}
+
+if ($_POST['usuario'] == "") {
+    $mensagem = '
+        <div class="notification is-danger">
+            <button class="delete"></button>
+            Usuário vazio
+        </div>';
+    die($mensagem);
+}
+
+if ($_POST['senha'] == "") {
+    $mensagem = '
+        <div class="notification is-danger">
+            <button class="delete"></button>
+            Senha vazia
+        </div>';
+    die($mensagem);
+}
+
+if($_GET['acao']=='login'){
+ (new ValidarUsuario())->retornar();
+
+    exit();
+}
+if($_GET['acao']=='cadastra'){
+    (new CadastrarUsuario())->retornar();
+   
+       exit();
+   }
+if($_GET['acao'=='excluir']){
+    (new ExcluirUsuario())->retornar();
+    header("Location:listar_estagios.php");
+}
