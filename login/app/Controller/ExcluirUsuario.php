474098e1cbd5f5524f051fb33849cc3fa422f683
Gabriel Mansur <144968802+Mansur-xtz@users.noreply.github.com>
2024-11-13 11:53:46 -0300

    Delete login directory

--- login/app/Controller/ExcluirUsuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__."/../Model/Estagiobanco.php";
-
-class ExcluirUsuario{
-    public function retornar(){
-      $usuarios = (new UserBanco())->excluirUsuario($_GET['id']);       
-
-    }
-}
